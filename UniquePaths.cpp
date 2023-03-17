039ca1a49901b9c7c39e2c8e54e8ea238055ae5a
Udit Kumar <58100192+kudit223@users.noreply.github.com>
2023-03-17 10:20:29 +0530

    Update UniquePaths.cpp

--- UniquePaths.cpp
+++ UniquePaths.cpp
@@ -28,10 +28,9 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         int dp[m+1][n+1];
-        dp[1][1]=1;
-        for(int i=2;i<m+1;i++)
+        for(int i=1;i<m+1;i++)
         dp[i][1]=1;
-        for(int j=2;j<n+1;j++)
+        for(int j=1;j<n+1;j++)
         dp[1][j]=1;
 
         for(int i=2;i<m+1;i++)

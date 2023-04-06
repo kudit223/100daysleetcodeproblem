5085891f7c8f09397aaccf826699a3f0166857ac
Udit Kumar <58100192+kudit223@users.noreply.github.com>
2023-04-07 00:21:37 +0530

    Create ContainsDuplicateII.cpp

---
+++ ContainsDuplicateII.cpp
@@ -0,0 +1,45 @@
+/*Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+0 <= k <= 105*/
+//time complexity:O(n)
+//space complexity:O(n)
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n=nums.size();
+        unordered_map<int,int>m;
+        for(int i=0;i<n;i++)
+        {
+            if(m.find(nums[i])==m.end())
+            m[nums[i]]=i;
+            else
+            {
+                if(abs(m[nums[i]]-i)<=k)
+                return true;
+                else
+                m[nums[i]]=i;
+            }
+        }
+        return false;
+    }
+};

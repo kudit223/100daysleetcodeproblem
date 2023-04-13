5249c4128ea3ad8b707b783b4b3ece54f09b667c
Udit Kumar <58100192+kudit223@users.noreply.github.com>
2023-04-13 19:25:23 +0530

    Create JumpGameIII.cpp

---
+++ JumpGameIII.cpp
@@ -0,0 +1,54 @@
+/*Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
+
+Notice that you can not jump outside of the array at any time.
+
+ 
+
+Example 1:
+
+Input: arr = [4,2,3,0,3,1,2], start = 5
+Output: true
+Explanation: 
+All possible ways to reach at index 3 with value 0 are: 
+index 5 -> index 4 -> index 1 -> index 3 
+index 5 -> index 6 -> index 4 -> index 1 -> index 3 
+Example 2:
+
+Input: arr = [4,2,3,0,3,1,2], start = 0
+Output: true 
+Explanation: 
+One possible way to reach at index 3 with value 0 is: 
+index 0 -> index 4 -> index 1 -> index 3
+Example 3:
+
+Input: arr = [3,0,2,1,2], start = 2
+Output: false
+Explanation: There is no way to reach at index 1 with value 0.
+ 
+
+Constraints:
+
+1 <= arr.length <= 5 * 104
+0 <= arr[i] < arr.length
+0 <= start < arr.length*/
+class Solution {
+public:
+    int solve(vector<int >&v,int start,vector<int>&dp,int n)
+    {
+        if(start<0||start>=n)
+        return 0;
+        if(dp[start]!=-1)
+        return dp[start];
+        if(v[start]==0||v[start]==0)
+        return 1;
+        dp[start]=0;
+        return dp[start]=solve(v,start-v[start],dp,n)||solve(v,start+v[start],dp,n);
+
+    }
+    bool canReach(vector<int>& arr, int start) {
+        int n=arr.size();
+        vector<int>dp(n,-1);
+        return solve(arr,start,dp,n);
+        
+    }
+};
